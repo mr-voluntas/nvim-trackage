e27e33b8234b3b56a1784985f7faf865004cbebd
voluntas <mr.voluntas@proton.me>
2025-02-15 02:11:09 +0000

    daily-tracking: fixed small plug with duplicate language time tracks showing on the floaty

--- lua/nvim-trackage/floating-window.lua
+++ lua/nvim-trackage/floating-window.lua
@@ -1,31 +1,42 @@
 local M = {}
 
-M.table_to_lines = function(trackage_data)
+local function to_string(trackage_totals)
 	local lines = {}
 
-	for _, data in pairs(trackage_data) do
-		for language, time in pairs(data) do
-			local hours = math.floor(time / 3600)
-			local minutes = math.floor((time % 3600) / 60)
-			local seconds = time % 60
+	for language, time in pairs(trackage_totals) do
+		local hours = math.floor(time / 3600)
+		local minutes = math.floor((time % 3600) / 60)
+		local seconds = time % 60
 
-			local time_str = {}
+		local time_str = {}
 
-			if hours > 0 then
-				table.insert(time_str, hours .. (hours == 1 and " hour" or " hours"))
-			end
-			if minutes > 0 then
-				table.insert(time_str, minutes .. (minutes == 1 and " min" or " mins"))
-			end
-			if seconds > 0 or (#time_str == 0) then
-				table.insert(time_str, seconds .. (seconds == 1 and " second" or " seconds"))
-			end
-
-			table.insert(lines, tostring(language) .. ": " .. tostring(table.concat(time_str, " ")))
+		if hours > 0 then
+			table.insert(time_str, hours .. (hours == 1 and " hour" or " hours"))
+		end
+		if minutes > 0 then
+			table.insert(time_str, minutes .. (minutes == 1 and " min" or " mins"))
+		end
+		if seconds > 0 or (#time_str == 0) then
+			table.insert(time_str, seconds .. (seconds == 1 and " second" or " seconds"))
 		end
 
-		return lines
+		table.insert(lines, tostring(language) .. ": " .. tostring(table.concat(time_str, " ")))
 	end
+	return lines
+end
+
+M.summarise_daily = function(trackage_data)
+	if not trackage_data and type(trackage_data) ~= "table" then
+		error("Expected table, got " .. type(trackage_data))
+	end
+
+	local trackage_totals = {}
+	for _, data in pairs(trackage_data) do
+		for language, time in pairs(data) do
+			trackage_totals[language] = (trackage_totals[language] or 0) + time
+		end
+	end
+	return to_string(trackage_totals)
 end
 
 M.open = function(trackage_data)
@@ -50,7 +61,7 @@ M.open = function(trackage_data)
 
 	local win = vim.api.nvim_open_win(buf, true, opts)
 
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, M.table_to_lines(trackage_data))
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, M.summarise_daily(trackage_data))
 
 	vim.api.nvim_buf_set_keymap(buf, "n", "<esc>", "<Cmd>bd!<CR>", { noremap = true, silent = true })
 
